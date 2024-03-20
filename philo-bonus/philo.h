c8331228d605322ad4239a54cb7372ac6e57f23f
maelBuchs <mael@buchs.fr>
2024-03-20 01:49:03 +0100

    dfsf

--- philo-bonus/philo.h
+++ philo-bonus/philo.h
@@ -15,28 +15,5 @@
 
 # include <stdio.h> // DONT FORGET TO REMOVE
 # include <sys/time.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <string.h>
-#include <unistd.h>
- typedef struct s_data
-  {
-	int		nb_philo;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		nb_must_eat;
-
-  }		t_data;
-
- typedef struct s_philo
- {
-	int		id;
-	struct s_data data;
-	pthread_mutex_t *forks;
-	pthread_mutex_t *print;
-	int *forks_status;
- } t_philo;
-
 
 #endif
--- philo/main.c
+++ philo/main.c
@@ -12,12 +12,76 @@
 
 #include "philo.h"
 
+void *hello_philo(void *p)
+{
+	t_philo *philo = (t_philo *)p;
+	if(philo->id % 2 == 0)
+		usleep(100000);
+	while (1)
+	{
+		pthread_mutex_lock(&philo->forks[philo->id]);
+			pthread_mutex_lock(philo->print);
+				philo->forks_status[philo->id] = 1;
+				printf("thread num %d a pris la fourchette de %d\n", philo->id, philo->id);
+			pthread_mutex_unlock(philo->print);
+			pthread_mutex_lock(philo->print);
+				pthread_mutex_lock(&philo->forks[(philo->id + 1) % 10]);
+		philo->forks_status[(philo->id + 1) % 10] = 1;
+		printf("thread num %d a pris la fourchette de %d\n", philo->id, (philo->id + 1) % 10);
+		pthread_mutex_unlock(philo->print);
+		pthread_mutex_lock(philo->print);
+		printf("thread num %d a MANGED la fourchette de %d et de %d\n", philo->id, philo->id, (philo->id + 1) % 10);
+		philo->forks_status[philo->id] = 1;
+		philo->forks_status[(philo->id + 1) % 10] = 0;
+		pthread_mutex_unlock(&philo->forks[(philo->id + 1) % 10]);
+		philo->forks_status[philo->id] = 0;
+		pthread_mutex_unlock(&philo->forks[philo->id]);
+		pthread_mutex_unlock(philo->print);
+
+		usleep(100000);
+	}
+
+}
+void *ft_calloc(size_t count, size_t size)
+{
+	void *ptr;
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	memset(ptr, 0, count * size);
+	return (ptr);
+}
+
 int main(void)
 {
-	struct timeval g_start_time;
+	long int i = 0;
+	pthread_mutex_t *forks = ft_calloc(sizeof(pthread_mutex_t), 10);
+	pthread_t *threads = ft_calloc(sizeof(pthread_t), 10);
+	t_philo *philo = ft_calloc(sizeof(t_philo), 10);
+	t_data *data = ft_calloc(sizeof(t_data),1);
+	pthread_mutex_t *print = ft_calloc(sizeof(pthread_mutex_t),1);
+	int *forks_status = ft_calloc(sizeof(int),	 10);
 
-
-	gettimeofday(&g_start_time, NULL);
-	printf("%ld seconds and %ld milliseconds\n", g_start_time.tv_sec, g_start_time.tv_usec);
+	while (i < 10)
+	{
+		philo[i].data = *data;
+		philo[i].id = i;
+		philo[i].forks = forks;
+		philo[i].print = print;
+		philo[i].forks_status = forks_status;
+		i++;
+	}
+	i = 0;
+	while(i < 10)
+	{
+		pthread_create(&threads[i], NULL, hello_philo, &philo[i]);
+		i++;
+	}
+	i = 0;
+	while(i < 10)
+	{
+		pthread_join(threads[i], NULL);
+		i++;
+	}
 	return 0;
 }
